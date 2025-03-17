0f04d596ecf1653b98b2981977f1c600ca3a2e4a
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-03-18 00:39:52 +0100

    [FIX] Code optimization

--- js/script.js
+++ js/script.js
@@ -9,21 +9,23 @@ let mykm = prompt("Quanti km devi percorrere?"); // Chiedo all'utente quanti km 
 if (isNaN(myage) || isNaN(mykm)) { // Controllo che l'input sia un numero
     alert("Input non valido"); // Informo l'utente che l'input non è valido
 }
-else if (myage < 18) { // Se l'età è minore di 18
-    let finalPrice = (mykm * (priceToKm * (1 - juniorDiscount))); // Calcolo il prezzo totale con scontistica
-    console.log (`Il costo del biglietto è ${finalPrice.toFixed(2)}€`); // Stampo il prezzo finale
-    alert("Hai diritto ad uno sconto del 20%"); // Informo l'utente dello sconto
-    alert(`Il costo del biglietto è ${finalPrice.toFixed(2)}€`); // Stampo il prezzo finale tramite alert
-}
-else if (myage > 65) { // Se l'età è maggiore di 65
-    let finalPrice = (mykm * (priceToKm * (1 - seniorDiscount))); // Calcolo il prezzo totale con scontistica
-    console.log (`Il costo del biglietto è ${finalPrice.toFixed(2)}€`); // Stampo il prezzo finale
-    alert("Hai diritto ad uno sconto del 40%"); // Informo l'utente dello sconto
-    alert(`Il costo del biglietto è ${finalPrice.toFixed(2)}€`); // Stampo il prezzo finale tramite alert
-}
-else if (myage >= 18 && myage<= 65){ // Se l'età è compresa tra 18 e 65
-    let finalPrice = mykm * priceToKm; // Calcolo il prezzo totale senza scontistiche
-    console.log (`Il costo del biglietto è ${finalPrice.toFixed(2)}€`); // Stampo il prezzo finale
-    alert("Non hai diritto a nessun sconto"); // Informo l'utente dello sconto
-    alert(`Il costoo del biglietto è ${finalPrice.toFixed(2)}€`); // Stampo il prezzo finale tramite alert
+else {
+    let price = mykm * priceToKm; // Calcolo il prezzo totale senza scontistiche
+    if (myage < 18){ // Controllo se l'utente è minorenne
+        let finalPrice = price * (1 - juniorDiscount); // Calcolo il prezzo totale con scontistica
+        console.log (`Il costo del biglietto è ${finalPrice.toFixed(2)}€`); // Stampo il prezzo finale
+        alert("Hai diritto ad uno sconto del 20%"); // Informo l'utente dello sconto
+        alert(`Il costo del biglietto è ${finalPrice.toFixed(2)}€`); // Stampo il prezzo finale tramite alert
+    } 
+    else if (myage > 65){ // Controllo se l'utente è over 65
+        let finalPrice = price * (1 - seniorDiscount); // Calcolo il prezzo totale con scontistica
+        console.log (`Il costo del biglietto è ${finalPrice.toFixed(2)}€`); // Stampo il prezzo finale
+        alert("Hai diritto ad uno sconto del 40%"); // Informo l'utente dello sconto
+        alert(`Il costo del biglietto è ${finalPrice.toFixed(2)}€`); // Stampo il prezzo finale tramite alert
+    } 
+    else { // Controllo se l'età è compresa tra 18 e 65
+        console.log (`Il costo del biglietto è ${price.toFixed(2)}€`); // Stampo il prezzo finale
+        alert("Non hai diritto a nessun sconto"); // Informo l'utente dello sconto
+        alert(`Il costo del biglietto è ${price.toFixed(2)}€`); // Stampo il prezzo finale tramite alert
+    }
 }
